3456fd37fc9885c5fc3714543a124934d30a985b
khushwant singh <khushwantrathore7865@gmail.com>
2022-01-22 17:32:43 +0530

    messages

--- EasShip/chat/views.py
+++ EasShip/chat/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from .models import Room, Message
 from django.http import HttpResponse, JsonResponse
 from django.contrib.auth.decorators import login_required
+from Customer.models import customer, Customer_profile
+from PatnerCompany.models import patnerComp, Comp_profile
 
 
 # Create your views here.
@@ -31,11 +33,21 @@ def send(request):
 
 @login_required(login_url='/')
 def getMessages(request, room):
-    muser=[]
+    muser = []
+    image = []
     room_details = Room.objects.get(name=room)
     useronline = request.user
     messages = Message.objects.filter(room=room_details.id)
     for m in messages:
         muser.append(m.user.user_name)
+        if m.user.is_customer:
+            c = customer.objects.get(user=m.user)
+            cp = Customer_profile.objects.get(cust=c)
+            image.append(cp.company_logo.url)
+        elif m.user.is_company:
+            p = patnerComp.objects.get(user=m.user)
+            pp = Comp_profile.objects.get(comp=p)
+            image.append(pp.company_logo.url)
+
     return JsonResponse(
-        {"messages": list(messages.values()), "muser": muser, "Ruser": useronline.user_name})
+        {"messages": list(messages.values()), "image": image, "muser": muser, "Ruser": useronline.user_name})
